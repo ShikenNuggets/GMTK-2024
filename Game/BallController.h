5fa034915f07c56c2560116589a5b9d599dac0cc
ShikenNuggets <carter.rennick@gmail.com>
2024-08-16 16:08:07 -0400

    Jump cooldown

--- Game/BallController.h
+++ Game/BallController.h
@@ -44,18 +44,22 @@ public:
 		rb->AddVelocity(direction * magnitude * moveSpeed * deltaTime_);
 
 		//Jumping
-		if(App::GetInput().GetButtonDown(SID("Jump"))){
+		jumpCooldownTimer -= deltaTime_;
+		if(App::GetInput().GetButtonDown(SID("Jump")) && jumpCooldownTimer <= 0.0f){
 			Vector3 oldVelocity = rb->GetVelocity();
 			rb->SetVelocity(oldVelocity.x, 0.0f, oldVelocity.z);
 
 			rb->AddForce(Vector3::Up() * jumpForce);
+			jumpCooldownTimer = jumpCooldownTime;
 		}
 	}
 
 private:
 	static constexpr float moveSpeed = 15.0f;
 	static constexpr float jumpForce = 7'500.0f;
+	static constexpr float jumpCooldownTime = 1.5f;
 
 	GameObject* cameraObj;
 	Rigidbody* rb;
+	float jumpCooldownTimer;
 };
