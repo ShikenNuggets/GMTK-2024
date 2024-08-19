4fb22f45fd544d1ed93e7db20a8d54a568ddcdf2
ShikenNuggets <carter.rennick@gmail.com>
2024-08-18 22:03:33 -0400

    Kitchen scene adjustments

--- Game/Scenes/KitchenScene.h
+++ Game/Scenes/KitchenScene.h
@@ -64,35 +64,58 @@ protected:
 		));
 
 		//12 blocks afterwards
-		static constexpr size_t numPlatforms = 22;
+		static constexpr size_t numPlatforms = 20;
 		int extraOffset = 0;
 		for(size_t i = 0; i < numPlatforms; i++){
 			const float platformDistance = 200.0f * i;
 			const float platformPos = -400.0f - platformDistance - extraOffset;
 			extraOffset += 20;
-
 			
 			CreateObject(new FloorObject(
 				Vector3(0.0f, 0.0f, platformPos + 15.0f),
 				Quaternion::Identity(),
 				Vector3(levelWidth, 1.0f, 100.0f),
-				SID("Invalid")
+				SID("BrickMaterial")
 			));
 
-			if(i % 2 == 0 || i == numPlatforms - 1){
+			const bool ramp = i % 2 == 0 || i == numPlatforms - 1;
+			if(ramp){
 				CreateObject(new RampObject(0.0f, platformPos));
-			}else{
+			}else if(i < numPlatforms / 2){
 				const float rand1 = Random::Range(-0.5f, 0.5f);
 				const float rand2 = Random::Range(-0.5f, 0.5f);
 				const float rand3 = Random::Range(-0.5f, 0.5f);
 				const float rand4 = Random::Range(-0.5f, 0.5f);
 				const float rand5 = Random::Range(-0.5f, 0.5f);
 
-				CreateObject(new CerealBoxObject(17.5f + rand1, platformPos)); //After stove gap 1
-				CreateObject(new CerealBoxObject(8.5f + rand2, platformPos)); //After stove gap 2
-				CreateObject(new CerealBoxObject(0.0f + rand3, platformPos)); //After stove gap 3
-				CreateObject(new CerealBoxObject(-8.5f + rand4, platformPos)); //After stove gap 4
-				CreateObject(new CerealBoxObject(-17.5f + rand5, platformPos)); //After stove gap 5
+				CreateObject(new CerealBoxObject(17.5f + rand1, platformPos));
+				CreateObject(new CerealBoxObject(8.5f + rand2, platformPos));
+				CreateObject(new CerealBoxObject(0.0f + rand3, platformPos));
+				CreateObject(new CerealBoxObject(-8.5f + rand4, platformPos));
+				CreateObject(new CerealBoxObject(-17.5f + rand5, platformPos));
+			}else{
+				const float rand1 = Random::Range(-0.5f, 0.5f);
+				const float rand2 = Random::Range(-0.5f, 0.5f);
+				const float rand3 = Random::Range(-0.5f, 0.5f);
+				const float rand4 = Random::Range(-0.5f, 0.5f);
+				const float rand5 = Random::Range(-0.5f, 0.5f);
+				const float rand6 = Random::Range(-1.0f, 1.0f);
+				const float rand7 = Random::Range(-1.0f, 1.0f);
+				const float rand8 = Random::Range(-1.0f, 1.0f);
+				const float rand9 = Random::Range(-1.0f, 1.0f);
+				const float rand10 = Random::Range(-1.0f, 1.0f);
+
+				CreateObject(new CerealBoxObject(17.5f + rand1, platformPos + 25.0f));
+				CreateObject(new CerealBoxObject(8.5f + rand2, platformPos + 25.0f));
+				CreateObject(new CerealBoxObject(0.0f + rand3, platformPos + 25.0f));
+				CreateObject(new CerealBoxObject(-8.5f + rand4, platformPos + 25.0f));
+				CreateObject(new CerealBoxObject(-17.5f + rand5, platformPos + 25.0f));
+
+				CreateObject(new CerealBoxObject(17.5f + rand6, platformPos - 25.0f));
+				CreateObject(new CerealBoxObject(8.5f + rand7, platformPos - 25.0f));
+				CreateObject(new CerealBoxObject(0.0f + rand8, platformPos - 25.0f));
+				CreateObject(new CerealBoxObject(-8.5f + rand9, platformPos - 25.0f));
+				CreateObject(new CerealBoxObject(-17.5f + rand10, platformPos - 25.0f));
 			}
 		}
 
